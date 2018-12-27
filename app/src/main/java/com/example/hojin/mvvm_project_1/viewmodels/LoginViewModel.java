bc543732b164835126568263e76a788d6cafbaad
jin kind <jinkind_ho@mymail.sutd.edu.sg>
2018-12-27 13:47:58 +0800

    upload app

---
+++ app/src/main/java/com/example/hojin/mvvm_project_1/viewmodels/LoginViewModel.java
@@ -0,0 +1,49 @@
+package com.example.hojin.mvvm_project_1.viewmodels;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.android.databinding.library.baseAdapters.BR;
+import com.example.hojin.mvvm_project_1.model.User;
+
+public class LoginViewModel extends BaseObservable {
+    private User user;
+
+
+    private String successMessage = "Login was successful";
+    private String errorMessage = "Email or Password not valid";
+
+    @Bindable
+    public String toastMessage = null;
+
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+
+    private void setToastMessage(String toastMessage) {
+
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    public LoginViewModel() {
+        user = new User("", "");
+    }
+
+    public void afterEmailTextChanged(CharSequence s) {
+        user.setEmail(s.toString());
+    }
+
+    public void afterPasswordTextChanged(CharSequence s) {
+        user.setPassword(s.toString());
+    }
+
+    public void onLoginClicked() {
+        if (user.isInputDataValid())
+            setToastMessage(successMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+}
